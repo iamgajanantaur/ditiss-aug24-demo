d7814c90d637ceb4b1e8cde027cda00409fc894d
Gajanan Taur <gajanan.taur96@gmail.com>
2024-12-26 11:37:53 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def root():
+    return "flask application version 1.1"
+
+# Ensure no non-printable characters in the following line
+app.run(host="0.0.0.0", port=4000)
+

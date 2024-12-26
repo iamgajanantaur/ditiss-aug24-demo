864fa59203596483d77573237410f97944979b5a
Gajanan Taur <gajanan.taur96@gmail.com>
2024-12-26 12:41:37 +0530

    v3

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-    return "flask application version 1.1"
+    return "flask application version 1.3"
 
 # Ensure no non-printable characters in the following line
 app.run(host="0.0.0.0", port=4000)
